dd9292d21a2129ecc1590a2b94d1e9574b162d43
최동준 <choedongjun@choedongjun-ui-MacBookAir.local>
2025-02-13 09:57:21 +0900

    chore: 전반적인 lint 설정 변경

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,22 @@
-import js from "@eslint/js";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import prettier from "eslint-config-prettier";
 
-export default {
-  ignores: ["dist"],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
-    parser: tsParser,
-    sourceType: "module",
-  },
-  plugins: {
-    react: react,
-    "react-hooks": reactHooks,
-    "react-refresh": reactRefresh,
-    "@typescript-eslint": tseslint,
-  },
-  extends: [
-    js.configs.recommended,
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-    "@typescript-eslint/no-unused-vars": "off",
-  },
-  settings: {
-    react: {
-      version: "detect",
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  prettier,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
+    ignores: ["node_modules/*", "dist/*", "build/*", "commitlint.config.js"],
   },
-};
+];
